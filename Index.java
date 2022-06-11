74572b7925e2429064a5e8855791c79d459800d7
Alexandre-CA <alexandre220404@gmail.com>
2022-06-11 18:51:39 -0300

    Login Accessibility

--- Index.java
+++ Index.java
@@ -4,21 +4,28 @@ public class Index {
 
     public static String accountLine=""; // Variavel global do cadastro
     public static void main(String[] args){
+        
+        callMain();
+        // System.out.println(accountLine);
+    }
+    public static void callMain(){
         int selectMethod;
         // String accountLine;
         Scanner tec = new Scanner(System.in);
         System.out.println("-----------------------------------");
         System.out.println("1 - Fazer Login");
         System.out.println("2 - Cadastro");
+        System.out.println("3 - Sair");
         System.out.println("-----------------------------------");
         System.out.print("Opcao -->");
         selectMethod = tec.nextInt();
         HelperClasses.cls();
-        while(selectMethod > 2 && selectMethod <1){
+        while(selectMethod > 3 && selectMethod <1){
             // System.out.println(selectMethod!=1);
             System.out.println("-----------------------------------");
             System.out.println("1 - Fazer Login");
             System.out.println("2 - Cadastro");
+            System.out.println("3 - Sair");
             System.out.println("-----------------------------------");
             System.out.print("Opcao -->");
             selectMethod = tec.nextInt();
@@ -33,10 +40,13 @@ public class Index {
             if(!accountLine.equals("")){
                 MenuPrincipal.menu(accountLine);
             }
+            else{
+                callMain();
+            }
             // Atualizar.atualizar(accountLine);
 
         }
-        else{
+        else if(selectMethod == 2){
             Cadastro.cadastro();
             HelperClasses.cls();
             System.out.println("-----------------------------------");
@@ -46,7 +56,13 @@ public class Index {
             if(!accountLine.equals("")){
                 MenuPrincipal.menu(accountLine);
             }
+            else{
+                callMain();
+            }
+
         }
-        // System.out.println(accountLine);
+        else{
+            return;
+        }
     }
 }
